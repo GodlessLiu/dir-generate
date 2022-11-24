ae3986d55c3b3ffa21186aeafea2dd46dde26380
victory <8515874+victory147@user.noreply.gitee.com>
2022-11-24 15:38:05 +0800

    init

---
+++ bin/index.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+const makefiles = require('../lib/makefiles.js');
+const log = require('../lib/log.js');
+const config = require('../config.json');
+const help = require('../lib/help');
+// DIRNAME
+const dir = process.cwd();
+
+const argv = yargs(hideBin(process.argv)).argv;
+
+// node bin/index.js -f filename
+const filename = argv.f;
+
+if (filename && typeof filename !== 'boolean') {
+  const configFiles = config.filesConfig[argv._[0]] ? config.filesConfig[argv._[0]] : config.filesConfig['0'];
+  makefiles.mkFiles(dir, filename, configFiles);
+  log.logSuccess(`文件${filename}创建完成`);
+} else {
+  log.logError('参数输入错误');
+}
